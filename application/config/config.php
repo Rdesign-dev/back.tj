6fe6bb93ec0c17e3289444b7f36cf8ad34171a0f
Rdesign-dev <mubarokfujianti@gmail.com>
2025-03-06 12:38:47 +0700

    fix: uncomment base_url configuration and improve session handling settings

--- application/config/config.php
+++ application/config/config.php
@@ -23,13 +23,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// @$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-// @$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-// @$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-// $config['log_path'] = APPPATH . 'logs/';
-// $config['sess_match_useragent'] = TRUE;
+@$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+@$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+@$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['log_path'] = APPPATH . 'logs/';
+$config['sess_match_useragent'] = TRUE;
 
-$config['base_url'] = 'http://localhost/back.tj/';
+// $config['base_url'] = 'http://localhost/back.tj/';
 
 /*
 |--------------------------------------------------------------------------
