fc90460b9c492aed891707a5d2304ee0dc2b4e61
pepetgoodboy <iqbaltw127@gmail.com>
2025-03-11 11:22:46 +0700

    fix: image history

--- application/views/topup/data.php
+++ application/views/topup/data.php
@@ -10,7 +10,7 @@
             <div class="col-auto">
                 <a href="<?= base_url('transaksi/saldo') ?>" class="btn btn-sm btn-primary btn-icon-split">
                     <span class="icon">
-                    <i class="fas fa-plus-circle"></i>
+                        <i class="fas fa-plus-circle"></i>
                     </span>
                     <span class="text">
                         Tambah Topup Saldo
@@ -36,27 +36,30 @@
                 $no = 1;
                 if ($tops) :
                     foreach ($tops as $top) :
+						// var_dump($top);
+						// die();
                         ?>
-                        <tr>
-                            <td><?= $no++; ?></td>
-                            <td>Rp <?= number_format($top->nominal, 0, ',', '.'); ?></td>
-                            <td><?= $top->metode; ?></td>
-                            <td>
-                                <?php if($top->bukti && $top->bukti != 'struk.png'): ?>
-                                    <img src="<?= base_url('../ImageTerasJapan/transaction_evidence/' . $top->bukti) ?>" 
-                                         alt="Bukti Transfer" width="100px" height="100px">
-                                <?php else: ?>
-                                    <span class="text-muted">Cash Payment</span>
-                                <?php endif; ?>
-                            </td>
-                            <td><?= $top->namamember; ?></td>
-                            <td><?= $top->nama; ?></td>
-                        </tr>
-                    <?php endforeach;
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td>Rp <?= number_format($top->nominal, 0, ',', '.'); ?></td>
+                    <td><?= $top->metode; ?></td>
+                    <td>
+                        <?php if($top->bukti != null): ?>
+                        <img src="http://localhost/ImageTerasJapan/transaction_proof/<?php echo $top->bukti; ?>"
+                            alt="Bukti Transfer" width="100px" height="100px">
+                        <?php else: ?>
+                        <img src="http://localhost/ImageTerasJapan/transaction_proof/struk.png" alt="Bukti Transfer"
+                            width="100px" height="100px">
+                        <?php endif; ?>
+                    </td>
+                    <td><?= $top->namamember; ?></td>
+                    <td><?= $top->nama; ?></td>
+                </tr>
+                <?php endforeach;
                     else : ?>
-                    <tr>
-                        <td colspan="6" class="text-center">Silahkan tambahkan data topup baru</td>
-                    </tr>
+                <tr>
+                    <td colspan="6" class="text-center">Silahkan tambahkan data topup baru</td>
+                </tr>
                 <?php endif; ?>
             </tbody>
         </table>
